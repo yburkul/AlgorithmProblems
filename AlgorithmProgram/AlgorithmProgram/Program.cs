23b9bdaa61fa8d1cdeecc965eeac9d52f549b435
yburkul <yburkul123@gmail.com>
2022-05-28 12:47:22 +0530

    Added:String Sorted order by using Insertion Sort

---
+++ AlgorithmProgram/AlgorithmProgram/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace AlgorithmProgram
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome in Alogrithm Program");
+            int Input;
+            do
+            {
+                Console.WriteLine("\n1:For InsertionSort");                
+                Console.WriteLine("0: For Exit Program");
+                Input = int.Parse(Console.ReadLine());
+                switch (Input)
+                {
+                    case 1:
+                        AlgorithmProblem insertionSort = new AlgorithmProblem();
+                        insertionSort.Insert();
+                        break;                   
+                    case 0:
+                        Console.WriteLine("Exit");
+                        break;
+                    default:
+                        Console.WriteLine("Input is Invalid");
+                        break;
+                }
+            }
+            while (Input != 0);            
+        }
+
+    }
+}
